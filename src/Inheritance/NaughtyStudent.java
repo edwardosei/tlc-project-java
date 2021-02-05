5f41ee44da1a8d6a6692bf5da8a0c19fda8ac0a2
edwardosei <edward.osei-nyarko@turntabl.io>
2021-02-05 12:21:07 +0000

    Added a NaughtyStudent class to check for student grade hacks.

---
+++ src/Inheritance/NaughtyStudent.java
@@ -0,0 +1,19 @@
+package Inheritance;
+
+import BasicJava.Student;
+
+import java.util.List;
+
+public class NaughtyStudent extends Student {
+
+    public NaughtyStudent(List<Double> studentGrades) {
+        super(studentGrades);
+    }
+
+    public double hack() {
+        double trueAverageGrade = this.getAverageGrade();
+        double increase = trueAverageGrade * 0.1;
+        double falseAverageGrade = trueAverageGrade + increase;
+        return falseAverageGrade;
+    }
+}
